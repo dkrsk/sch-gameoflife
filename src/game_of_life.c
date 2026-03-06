7c5897e6deab4cf7accc6388e552cbf0fe91d1f4
Arcturus Jaleesa <dedaireks@gmail.com>
2026-03-06 15:45:55 +0300

    add speed input changes in develop

--- src/game_of_life.c
+++ src/game_of_life.c
@@ -7,12 +7,20 @@ int main()
 {
     struct cell** world = init_world();
     struct cell** wolrd2 = init_world();
-    int speed = 40;
+    interactive();
+    int speed = 200;
     load_pattern(world);
     int input = handle_input();
-    while (input != 10)
-    {
+    while (input != INPUT_QUIT)
+    {   
+        timeout(speed);
         int input = handle_input();
+        if (input == INPUT_FASTER){
+            speed -= 20;
+        }
+        if (input == INPUT_SLOWER){
+            speed += 20;
+        } 
         render(world, speed);
         update(&world, &wolrd2);
     }
