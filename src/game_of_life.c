b7d2ec045f416684cfb90d2e69ae7857a708d373
Arcturus Jaleesa <dedaireks@gmail.com>
2026-03-06 14:50:19 +0300

    add init func body in feature/main

--- src/game_of_life.c
+++ src/game_of_life.c
@@ -2,4 +2,17 @@
 #include "gol_io.h"
 #include "gol_struct.h"
 
-int main() { return 0; }
+int main()
+{
+    struct cell** world = init_world();
+    load_pattern(&world);
+    int input = handle_input();
+    while (input != 10)
+    {
+        int input = handle_input();
+        render(world);
+        update(&world);
+    }
+    destroy_world(&world);
+    return 0;
+}
